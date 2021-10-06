575ff26df5bf1e273b4f4893130fa2775e6a0db1
Matias Heikkilä <matias@three.consulting>
2021-10-07 00:54:50 +0300

    Hide footer when not logged in

--- hu/settings.py
+++ hu/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_URL = "auth/login"
+LOGIN_URL = "/auth/login"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
